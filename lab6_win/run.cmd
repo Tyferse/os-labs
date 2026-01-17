ddcc07057e92ffb5ba535c9d33d625c6abb1ee94
Tyferse <qwerty482803@gmail.com>
2026-01-17 12:18:26 +1000

    lab6 windows charts work

--- lab6_win/run.cmd
+++ lab6_win/run.cmd
@@ -1,11 +1,9 @@
 @echo off
 echo === Запуск процесса сборки ===
 
-setlocal
-
 REM Установка переменных для Qt
 set QT_INSTALL="C:\Qt\Qt-6.10.1"
-set QT_PLUGIN_PATH="%QT_INSTALL%\plugins"
+@REM set QT_PLUGIN_PATH="%QT_INSTALL%\plugins"
 
 REM Переход в директорию проекта
 set PROJ_DIR=%~dp0
@@ -22,14 +20,12 @@ copy "%QT_INSTALL%\bin\Qt6Core.dll" .
 copy "%QT_INSTALL%\bin\Qt6Gui.dll" .
 copy "%QT_INSTALL%\bin\Qt6Widgets.dll" .
 
-@REM if not exist "platforms" (
-@REM     mkdir platforms
-@REM     copy "%QT_INSTALL%\plugins\platforms\qwindows.dll" platforms\
-@REM )
+if not exist "platforms" (
+    mkdir platforms
+    copy "%QT_INSTALL%\plugins\platforms\qwindows.dll" platforms\
+)
 
-cmake -G "MinGW Makefiles" -DCMAKE_PREFIX_PATH="%QT_INSTALL%" ..
+cmake -G "MinGW Makefiles" ..
 mingw32-make
 
-QtTest.exe
-
-endlocal
+.\QtTest.exe
